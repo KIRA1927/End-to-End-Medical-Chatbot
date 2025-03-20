474d0789066c8ac310914c733ee887769a7521f9
KIRA1927 <patilvishwajeet158@gmail.com>
2025-03-21 01:02:51 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = 'Vishwajeet',
+    author_email = 'codeblack2703@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
